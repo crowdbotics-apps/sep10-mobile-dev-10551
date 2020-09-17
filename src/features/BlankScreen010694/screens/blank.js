ba8b3db6866dafe55e57fb3cac004435d813c9ff
Crowdbotics <team@crowdbotics.com>
2020-09-17 18:00:10 +0000

    Update components

--- src/features/BlankScreen010694/screens/blank.js
+++ src/features/BlankScreen010694/screens/blank.js
@@ -22,7 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = { Toggle_9: true }
+  state = {}
 
   render = () => (
     <View
@@ -58,22 +58,29 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <Text
+      <View
         style={{
+          width: "100%",
+          height: "100%",
           marginLeft: 0,
           marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
           overflow: "visible",
-          alignSelf: "center",
           fontSize: 12,
           color: "#000000",
           backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
+          alignItems: "stretch",
+          alignContent: "stretch",
           borderColor: "#000000",
           borderStyle: "solid",
           borderWidth: 0,
@@ -82,122 +89,44 @@ export class _Blank extends React.Component {
           borderTopWidth: 0,
           borderBottomWidth: 0,
           borderRadius: 0,
-          textAlign: "center",
-          textAlignVertical: "center",
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
+          backgroundSize: "auto"
         }}
       >
-        Sample text content
-      </Text>
-      <Button
-        textStyle={{
-          fontSize: 12,
-          color: "#000000",
-          textAlign: "center",
-          fontFamily: "Roboto-Regular"
-        }}
-        style={{
-          width: "50%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          alignSelf: "center",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#3366FF",
-          fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textAlign: "center",
-          textAlignVertical: "center",
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        onPress={() => alert("Pressed!")}
-      >
-        Press me!
-      </Button>
-      <Slider
-        value={50}
-        minimumValue={0}
-        maximumValue={100}
-        step={1}
-        disabled={false}
-        maximumTrackTintColor="#E4E7ED"
-        minimumTrackTintColor="#3366FF"
-        thumbTintColor="#3366FF"
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-      />
-      <Toggle
-        text="switch ON/OFF"
-        activeColor="#409EFF"
-        inactiveColor="#C0CCDA"
-        disabled={false}
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        checked={this.state.Toggle_9}
-        onChange={nextChecked => this.setState({ Toggle_9: nextChecked })}
-      />
+        <Text
+          style={{
+            marginLeft: 0,
+            marginRight: 0,
+            marginTop: 5,
+            marginBottom: 5,
+            paddingLeft: 5,
+            paddingRight: 5,
+            paddingTop: 5,
+            paddingBottom: 5,
+            overflow: "visible",
+            fontSize: 12,
+            color: "#000000",
+            backgroundColor: "#ffffff",
+            fontStyle: "normal",
+            fontWeight: "normal",
+            borderColor: "#000000",
+            borderStyle: "solid",
+            borderWidth: 0,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+            borderRadius: 0,
+            textAlign: "left",
+            textAlignVertical: "center",
+            textDecorationLine: "none",
+            textTransform: "none",
+            lineHeight: 12,
+            letterSpacing: 0
+          }}
+        >
+          Sample text content
+        </Text>
+      </View>
     </View>
   )
 }
